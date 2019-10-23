35feaa5924daa32e98439827acfdd55f5f6cf4af
Aida Pirahmadi <Aidapirahmadi@yahoo.com>
2019-10-23 14:21:29 -0700

    rev1

--- apps/coaching_app/urls.py
+++ apps/coaching_app/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from . import views
                     
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^regprocess$', views.user),
+    url(r'^login_page$', views.login_page),
+    url(r'^regprocess$', views.user_process),
     url(r'^registration$', views.registration),
     url(r'^loginprocess$', views.login_process),
     url(r'^login$', views.login),
-    url(r'^logout$', views.logout)
+    url(r'^logout$', views.logout),
+    url(r'^$', views.home_page)
 ]
--- apps/coaching_app/views.py
+++ apps/coaching_app/views.py
@@ -4,12 +4,15 @@ import bcrypt
 from .models import *
 
 
-def index(request):
+def home_page(request):
+    return render(request, 'coaching_app/index.html')
+
+
+def login_page(request):
     return render(request, 'coaching_app/login_reg.html')
 
 
-def user(request):
-    # User processor
+def user_process(request):
     errors = User.objects.validator(request.POST)
     if len(errors) > 0:
         for key, value in errors.items():
@@ -20,18 +23,20 @@ def user(request):
         email = request.POST["email"]
         matched_user = User.objects.filter(username=request.POST["username"])
         if len(matched_user) > 0:
-            messages.error(request, 'Username unavailable', extra_tags="register")
+            messages.error(request, 'Username unavailable',
+                           extra_tags="register")
             return redirect('/#toregister')
-        pw_hash = bcrypt.hashpw(request.POST["password"].encode(), bcrypt.gensalt())
+        pw_hash = bcrypt.hashpw(
+            request.POST["password"].encode(), bcrypt.gensalt())
         new_user = User.objects.create(
             username=username, email=email, password=pw_hash)
-        request.session["new_user_id"] = new_user.id  
+        request.session["new_user_id"] = new_user.id
     return redirect('/registration')
 
 
 def registration(request):
     context = {
-        "reg_user": User.objects.get(id = request.session["new_user_id"]),
+        "reg_user": User.objects.get(id=request.session["new_user_id"]),
     }
     return render(request, 'coaching_app/success.html', context)
 
@@ -40,25 +45,27 @@ def login_process(request):
     matched_user = User.objects.filter(username=request.POST['username'])
     print(matched_user)
     if len(matched_user) < 1:
-        messages.error(request, 'Email or password does not match', extra_tags="login")
-        return redirect('/')
+        messages.error(
+            request, 'Email or password does not match', extra_tags="login")
+        return redirect('/login_page')
     if bcrypt.checkpw(request.POST['password'].encode(), matched_user[0].password.encode()):
         request.session['username'] = request.POST['username']
         request.session['username'] = request.POST['username']
         return redirect('/login')
     else:
-        messages.error(request, 'Email or password do not match', extra_tags="login")
-        return redirect('/')
-    return redirect('/')
+        messages.error(request, 'Email or password do not match',
+                       extra_tags="login")
+        return redirect('/login_page')
+    return redirect('/login_page')
 
 
 def login(request):
     context = {
-        "reg_user": User.objects.filter(username = request.session["username"])[0]
+        "reg_user": User.objects.filter(username=request.session["username"])[0]
     }
     return render(request, 'coaching_app/success.html', context)
 
+
 def logout(request):
     request.session.clear()
-    return redirect('/')
-
+    return redirect('/login_page')
