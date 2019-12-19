27afade459632c7e2aad439e9bb07c22a84a14ef
Reed Crawley <reedcrawley@Reeds-MacBook-Air.local>
2019-12-18 16:25:09 -0800

    message

--- apps/coaching_app/urls.py
+++ apps/coaching_app/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-                    
+
 urlpatterns = [
     url(r'^login_page$', views.login_page),
     url(r'^regprocess$', views.user_process),
@@ -18,10 +18,6 @@ urlpatterns = [
     url(r'^user/edit/(?P<userid>\w+)$', views.edit_account),
     # url(r'^createpost$', views.createpost),
     url(r'^sampleworkout$', views.sampleworkout),
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1dc7a371d92d3bdf3809f9e8b0621068cf7a50a5
     url(r'^search', views.search),
     # url(r'^createpost$', views.createpost)
     url(r'^search', views.search),
